a84f911bed78f14bc4f83447d6bed1a11ece8661
EC2 Default User <ec2-user@ip-172-31-30-65.us-east-2.compute.internal>
2019-03-08 05:26:32 +0000

    part 2 complete

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -1,25 +1,10 @@
+Given /the following movies exist:$/ do |movies_table|
+    movies_table.hashes.each do |hash|
+        Movie.create!(title: hash['title'], rating: hash['rating'], release_date: hash['release_date'], director: hash['director'])
+    end
+end 
 
-Given /the following movies exist/ do |movies_table|
-  movies_table.hashes.each do |movie|
-    Movie.create movie
-  end
-end
-
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  expect(page.body.index(e1) < page.body.index(e2))
-end
-
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
-  end
-end
-
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  Movie.all.each do |movie|
-    step %{I should see "#{movie.title}"}
-  end
+Then /^the director of "(.*)" should be "([^"]*)"$/ do |movie_name, value|
+  expect(page).to have_content(movie_name)
+  expect(page.body).to match(/Director:\s#{value}/)
 end
